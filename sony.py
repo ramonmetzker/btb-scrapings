fdec8daa94acf6bbf8d1d3b898b29ebba6cae451
Ramon Metzker <ramonmetzker12@gmail.com>
2025-08-03 22:38:28 -0400

    fix: change supabase to turso

--- sony.py
+++ sony.py
@@ -1,12 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
-from supabase import create_client, Client
 import os
-
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_ANON_KEY = os.getenv("SUPABASE_ANON_KEY")
-
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_ANON_KEY)
+import libsql
 
 def convert_price(price):
     normalized = price.replace("R$", "").replace(".", "").replace(",", ".").strip()
@@ -14,6 +9,14 @@ def convert_price(price):
 
 def parse_discount(discount_str):
     return float(discount_str.replace("%", "").strip()) / 100
+
+db_url = os.getenv("TURSO_DB_URL")
+db_token = os.getenv("TURSO_AUTH_TOKEN")
+
+conn = libsql.connect(
+    syncurl=db_url,
+    auth_token=db_token
+)
 
 url = "https://store.playstation.com/pt-br/category/3f772501-f6f8-49b7-abac-874a88ca4897/1?FULL_GAME=storeDisplayClassification"
 store = "https://store.playstation.com"
@@ -28,20 +31,16 @@ if response.status_code == 200:
     for ol in ols:
         lis = ol.find_all('li')
         li = lis[-1]
-
         span = li.find('span')
-
         total_pages = int(span.getText())
         print(f"[TOTAL PAGES]: {total_pages}")
-        print(f"==================================")
+        print("==================================")
 
-    for i in range(1,total_pages + 1):
+    for i in range(1, total_pages + 1):
         page = requests.get(f"https://store.playstation.com/pt-br/category/3f772501-f6f8-49b7-abac-874a88ca4897/{i}?FULL_GAME=storeDisplayClassification")
-
         soup = BeautifulSoup(page.text, 'html.parser')
 
         grid = soup.select('.psw-grid-list')[0]
-
         cards = grid.find_all('li')
 
         values = []
@@ -51,18 +50,31 @@ if response.status_code == 200:
             game_art = card.select_one('[data-qa$="#image#image-no-js"]').attrs.get('src')
             game_type = card.select_one('[data-qa$="#product-type"]').getText() if card.select_one('[data-qa$="#product-type"]') else "Jogo completo"
             discount_price = card.select_one('[data-qa$="#price#display-price"]').getText()
-            original_price = card.select_one('[data-qa$="#price-strikethrough"]').getText() if card.select_one('[data-qa$="#price-strikethrough"]') else None
+            original_price = card.select_one('[data-qa$="#price-strikethrough"]').getText() if card.select_one('[data-qa$="#price-strikethrough"]') else discount_price
             discount = card.select_one('[data-qa$="#discount-badge#text"]').getText()
             platforms = card.select_one('[data-qa$="#game-art"]').select('span.psw-platform-tag')
             all_platforms = ", ".join(span.getText() for span in platforms)
             link = store + card.select_one('a.psw-link').attrs.get('href')
-            values.append((title,game_type,parse_discount(discount),convert_price(discount_price),convert_price(original_price),game_art,all_platforms, link))
 
-        cols = ['title','type','discount','price','original_price','art','platforms','link']
-        records = [dict(zip(cols, row)) for row in values]
-        unique_records = [dict(t) for t in {tuple(sorted(d.items())) for d in records}]
-        supabase.table('sony').upsert(unique_records, on_conflict="title,price,link").execute()
+            values.append((title, game_type, parse_discount(discount), convert_price(discount_price), convert_price(original_price), game_art, all_platforms, link))
 
-        print(f"Done Page {i}")
+        print(f"[PAGE {i}] Total: {len(values)} games")
 
-    print("End")
+        for record in values:
+            conn.execute(
+                """
+                INSERT INTO sony (title, type, discount, price, original_price, art, platforms, link)
+                VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8)
+                ON CONFLICT(title, price, link) DO UPDATE SET
+                    id=excluded.id,
+                    discount=excluded.discount,
+                    original_price=excluded.original_price,
+                    art=excluded.art,
+                    platforms=excluded.platforms;
+                """,
+                record
+            )
+
+        print(f"✔️ Done Page {i}")
+
+    print("✅ All Done")
